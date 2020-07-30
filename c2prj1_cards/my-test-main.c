9288038c9595b3e9b7fa4ffa19deaf67b489e926
Coursera Learner <nobody@nowhere.nul>
2020-07-30 14:39:02 +0000

    cards

--- c2prj1_cards/my-test-main.c
+++ c2prj1_cards/my-test-main.c
@@ -1,5 +1,15 @@
 #include "cards.h"
+#include <stdlib.h>
+#include <stdio.h>
 
-int main(void) {
+int main() {
+    card_t c;
+    c.value = 14;
 
+    c.suit = 2;
+    //char m  = value_letter(c);
+    //char z = suit_letter(c);
+    //assert_card_valid(c);
+    print_card(c);
+  return 0;
 }
