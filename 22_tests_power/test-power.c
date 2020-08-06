ddbd3c8c7265b3a263c2625e25b2e6d1fa59f740
Coursera Learner <nobody@nowhere.nul>
2020-08-06 18:23:28 +0000

    tsets

---
+++ 22_tests_power/test-power.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned power(unsigned x, unsigned y);
+int main()
+{
+  if(power(0, 0) != 1){
+    return EXIT_FAILURE;
+  }
+  if(power(1, 0) != 1){
+    return EXIT_FAILURE;
+  }
+  if(power(0, 3) != 0){
+    return EXIT_FAILURE;
+  }
+  if(power(2, 4) != 16){
+    return EXIT_FAILURE;
+  }
+  if(power(3.14, 3.8) != 27){
+    return EXIT_FAILURE;
+  }
+  if(power(5, 0) != 1){
+    return EXIT_FAILURE;
+  }
+  if(power(1, 0) != 1){
+    return EXIT_FAILURE;
+  }
+  if(power(6, 1) != 6){
+    return EXIT_FAILURE;
+  }
+  if(power(6, 1) != 6){
+    return EXIT_FAILURE;
+  }
+  if(power('a', 'b') != 1993115841){
+    return EXIT_FAILURE;
+  }
+  if(power(-2, 4) != 16){
+    return EXIT_FAILURE;
+  }
+  if(power(-2, 3) != -8){
+    return EXIT_FAILURE;
+  }
+  if(power(2, -4) != 0){
+    return EXIT_FAILURE;
+  }
+  if(power(-2, -4) != 0){
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
+}
