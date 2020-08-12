b9fbe40674cbf2cb4e695821ffc85be6668ebc3f
Coursera Learner <nobody@nowhere.nul>
2020-08-12 18:08:31 +0000

    wordsort

--- 30_sort_lines/sortLines.c
+++ 30_sort_lines/sortLines.c
@@ -18,7 +18,55 @@ void sortData(char ** data, size_t count) {
 
 int main(int argc, char ** argv) {
   
-  //WRITE YOUR CODE HERE!
-  
+  char ** lines = NULL;
+  char * curr = NULL;
+  size_t sz = 0; //might be bug
+  size_t j = 0;
+
+  if(argc == 1){
+    while(getline(&curr, &sz, stdin) >= 0){
+      lines = realloc(lines, (j+1) * sizeof(*lines));
+      lines[j] = curr;
+      curr = NULL;
+      j++;
+    }
+    free(curr);
+    sortData(lines, j);
+    for(size_t j2 = 0; j2 < j; j2++){
+      printf("%s", lines[j2]);
+      free(lines[j2]);
+    }
+    free(lines);
+  }
+  if(argc > 1){
+    for(int i = 1; i < argc; i++){
+      FILE* f = fopen(argv[i], "r");
+      if (f == NULL) {
+	perror("Could not open file");
+	return EXIT_FAILURE;
+      }
+      while(getline(&curr, &sz, f) >= 0){
+	lines = realloc(lines, (j+1) * sizeof(*lines));
+	lines[j] = curr;
+	curr = NULL;
+	j++;
+      }
+      free(curr);
+      sortData(lines, j);
+      for(size_t j2 = 0; j2 < j; j2++){
+	printf("%s", lines[j2]);
+	free(lines[j2]);
+      }
+      free(lines);
+      if (fclose(f) != 0) {
+	perror("Failed to close the input file!");
+	return EXIT_FAILURE;
+      }
+      j = 0;
+      sz = 0;
+      curr = NULL;
+      lines = NULL;
+    }
+  }
   return EXIT_SUCCESS;
 }
