40df32647c1f87dda2969179bfbd3b122f6ba382
Coursera Learner <nobody@nowhere.nul>
2020-08-14 18:34:06 +0000

    kvs

--- 32_kvs/kv.c
+++ 32_kvs/kv.c
@@ -3,20 +3,53 @@
 #include <string.h>
 #include "kv.h"
 
-
+char* splitLine(char* line) {
+  char* value = strchr(line, '=');
+  *value = '\0';
+  value++;
+  value[strlen(value) - 1] = '\0';
+  return value;
+}
 
 kvarray_t * readKVs(const char * fname) {
-  //WRITE ME
+  FILE * f = fopen(fname, "r");
+  kvarray_t * kvarray = malloc(sizeof(*kvarray));
+  kvarray->pair = malloc(sizeof(*kvarray->pair));
+  kvarray->size = 0;
+  char * line = NULL;
+  size_t sz = 0;
+  int count = 1;
+  int elem = 0;
+  while(getline(&line, &sz, f) != -1){
+    if(line[0] == '\n'){
+      continue;
+    }
+    kvarray->pair = realloc(kvarray->pair, ((elem+1) * sizeof(*kvarray->pair)));
+    kvarray->pair[elem] = malloc(sizeof(*kvarray->pair[elem]));
+    kvarray->pair[elem]->value = splitLine(line);
+    kvarray->pair[elem]->key = line;
+    elem++;
+    line = NULL;
+
+  }
+  kvarray->size = elem;
+  free(line);
+  return kvarray;
 }
 
 void freeKVs(kvarray_t * pairs) {
-  //WRITE ME
+  for(int i = 0; i < pairs->size; i++){
+    free(pairs->pair[i]->key);
+    free(pairs->pair[i]);
+  }
+  free(pairs->pair);
+  free(pairs);
 }
 
 void printKVs(kvarray_t * pairs) {
-  //WRITE ME
+  for(int i = 0; i < pairs->size; i++){
+    printf("key = '%s' value = '%s'\n", pairs->pair[i]->key, pairs->pair[i]->value);
+  }
 }
 
-char * lookupValue(kvarray_t * pairs, const char * key) {
-  //WRITE ME
-}
+
--- 32_kvs/kv_test.c
+++ 32_kvs/kv_test.c
@@ -4,13 +4,23 @@
 #include "kv.h"
 
 #define NUM_LOOKUPS 5
+
+char * lookupValue(kvarray_t * pairs, const char * key) {
+  for(int i = 0; i < pairs->size; i++){
+    if(strcmp(pairs->pair[i]->key, key) == 0){
+      return pairs->pair[i]->value;
+    }
+  }
+  return NULL;
+}
+
 int main(void) {
   kvarray_t * array = readKVs("test.txt");
   printf("Printing all keys\n\n");
   printKVs(array);
   char *tests[NUM_LOOKUPS] = {"banana", "grapes", "cantaloupe", "lettuce", "orange"};
   for (int i = 0; i < NUM_LOOKUPS; i++) {
-    printf("lookupValue('%s')=%s\n", tests[i], lookupValue(array,tests[i]));
+     printf("lookupValue('%s')=%s\n", tests[i], lookupValue(array,tests[i]));
   }
   freeKVs(array);
   return EXIT_SUCCESS;
