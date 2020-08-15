0e0413983a0bdb23c20fbb97cc16ab564bec9702
Coursera Learner <nobody@nowhere.nul>
2020-08-15 15:35:29 +0000

    counts12

--- 33_counts/counts_test.c
+++ 33_counts/counts_test.c
@@ -6,11 +6,11 @@
 #define NUM_TESTS 12
 
 int main(void) {
-  char * testData[NUM_TESTS] = {"apple", "banana", NULL,"apple",
-				"frog","sword","bear",NULL,
-				"frog","apple", "zebra", "knight"};
+  char * testData[10] = {"apple", "banana","apple",
+				"frog","sword","sword",
+				"frog","sword", "zebra", "knight"};
   counts_t * testCounts= createCounts();
-  for(int i =0; i < NUM_TESTS; i++) {
+  for(int i =0; i < 10; i++) {
     addCount(testCounts,testData[i]);
   }
   printCounts(testCounts, stdout);
